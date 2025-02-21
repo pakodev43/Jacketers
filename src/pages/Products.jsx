76c174b9d7f15710f63180d393821e5356d1421a
dev <dev@git.com>
2025-02-22 02:28:07 +0500

    Minor fix

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import Header from '../components/Header.jsx'
-import Product from '../components/product.jsx'
+import Product from '../components/Product.jsx'
 import Footer from '../components/Footer.jsx'
 
 const Products = () => {
